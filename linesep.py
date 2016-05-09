fe78335e4f469e22f9a1de7a1e5ddd52021a7f0f
John T. Wodder II <git@varonathe.org>
2016-05-09 12:37:25 +0000

    Use three public functions instead of one

--- linesep.py
+++ linesep.py
@@ -1,18 +1,6 @@
-STARTER = -1
-SEPARATOR = 0
-TERMINATOR = 1
-
-def readlines(fp, sep, mode=TERMINATOR, retain=True, size=512):
-    if mode < 0:
-        return _readlines_start(fp, sep, retain, size)
-    elif mode == 0:
-        return _readlines_sep(fp, sep, size)
-    else:
-        return _readlines_term(fp, sep, retain, size)
-
-def _readlines_start(fp, sep, retain=True, size=512):
+def read_begun(fp, sep, retain=True, size=512):
     # Omits empty leading entry
-    entries = _readlines_sep(fp, sep, size=size)
+    entries = read_separated(fp, sep, size=size)
     e = next(entries)
     if e:
         yield e
@@ -21,7 +9,7 @@ def _readlines_start(fp, sep, retain=True, size=512):
             e = sep + e
         yield e
 
-def _readlines_sep(fp, sep, size=512):
+def read_separated(fp, sep, size=512):
     buff = ''
     for chunk in iter(lambda: fp.read(size), ''):
         buff += chunk
@@ -31,7 +19,7 @@ def _readlines_sep(fp, sep, size=512):
             yield l
     yield buff
 
-def _readlines_term(fp, sep, retain=True, size=512):
+def read_terminated(fp, sep, retain=True, size=512):
     # Omits empty trailing entry
     buff = ''
     for chunk in iter(lambda: fp.read(size), ''):
