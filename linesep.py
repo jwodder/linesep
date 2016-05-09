1d2ef081c6c2f21dedc15a91414ea455e73932b4
John T. Wodder II <git@varonathe.org>
2016-05-09 13:53:22 +0000

    Updated/rewrote `read_terminated`

--- linesep.py
+++ linesep.py
@@ -1,18 +1,15 @@
-from   itertools import tee
 import re
-import sys
 
-if sys.version_info[0] == 2:
-    from itertools import izip
-else:
-    izip = zip
-
-def _pairwise(iterable):
-    # from <https://docs.python.org/2.7/library/itertools.html#recipes>
-    """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
-    a, b = tee(iterable)
-    next(b, None)
-    return izip(a, b)
+def join_pairs(iterable):
+    while True:
+        a = next(iterable)
+        try:
+            b = next(iterable)
+        except StopIteration:
+            yield a
+            return
+        else:
+            yield a + b
 
 def read_begun(fp, sep, retain=True, size=512):
     # Omits empty leading entry
@@ -21,11 +18,9 @@ def read_begun(fp, sep, retain=True, size=512):
     if e:
         yield e
     if retain:
-        for a,b in _pairwise(entries):
-            yield a+b
-    else:
-        for e in entries:
-            yield e
+        entries = join_pairs(entries)
+    for e in entries:
+        yield e
 
 def read_separated(fp, sep, retain=True, size=512):
     if not isinstance(sep, re.RegexObject):
@@ -44,14 +39,13 @@ def read_separated(fp, sep, retain=True, size=512):
 
 def read_terminated(fp, sep, retain=True, size=512):
     # Omits empty trailing entry
-    buff = ''
-    for chunk in iter(lambda: fp.read(size), ''):
-        buff += chunk
-        lines = buff.split(sep)
-        buff = lines.pop()
-        for l in lines:
-            if retain:
-                l += sep
-            yield l
-    if buff:
-        yield buff
+    prev = None
+    entries = read_separated(fp, sep, retain=retain, size=size)
+    if retain:
+        entries = join_pairs(entries)
+    for e in entries:
+        if prev is not None:
+            yield prev
+        prev = e
+    if prev:
+        yield prev
